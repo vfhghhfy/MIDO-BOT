aaf969a5cdc402373bdea9c454f18a2913359caf
flash005 <fbashm085@gmail.com>
2024-09-28 02:42:39 +0200

    Update تطبيق.js

--- plugins/تطبيق.js
+++ plugins/تطبيق.js
@@ -1,18 +1,20 @@
 import {search, download} from 'aptoide-scraper';
 const handler = async (m, {conn, usedPrefix: prefix, command, text}) => {
- if (!text) throw `*֎╎اكـتـب اسـم تـطـبـيـق لـتـنـزيـلـه❤️‍🩹*`;
+ if (!text) throw `معشوق الجماهير هذا الأمر خاص بتحميل التطبيقات المجانية والمدفوعة منها نكتب هكذا على سبيل المثال \n*.apk facebbok lite*`;
   try {    
     const searchA = await search(text);
     const data5 = await download(searchA[0].id);
-    let response = `*〖📲┇تـحـمـيـل الـتـطـبـيـقـات┇📲〗*\n\n֎╎اسـم الـتـطبـيـق📌┇ ${data5.name}\n֎╎اسـم الـطـرد📦┇ ${data5.package}\n֎╎الانـتـاج🕒┇ ${data5.lastup}\n֎╎الـحـجـم📥┇ ${data5.size}`
+    let response = `📲 تحميل التطبيقات 📲\n\n📌 *اسم التطبيق:* ${data5.name}\n📦 *الباكيدج:* ${data5.package}\n🕒 *تحذيث رقم:* ${data5.lastup}\n📥 *حجم التطبيق:* ${data5.size}\n\nجاري تنزيل التطبيق  \n`
     await conn.sendMessage(m.chat, {image: {url: data5.icon}, caption: response}, {quoted: m});
  if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
-      return await conn.sendMessage(m.chat, {text: '*֎╎حـجـم الـمـلـف كـبـيـر جـدا لـذا لـن يـتـم ارسـالـه⛔*'}, {quoted: m});
+      return await conn.sendMessage(m.chat, {text: '*[ 😁 ]الملف كبير جدًا لذا لن يتم إرساله.'}, {quoted: m});
     }
     await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: m});
   } catch {
-    throw `֎╎ايـرور❗`;
+    throw `*[😒] خطأ، لم يتم العثور على نتائج لبحثك.*`;
   }    
 };
-handler.command = /^(apkmod|apk|apek|تطبيق)$/i;
+handler.help = ["apk"]
+handler.tags = ["applications"]
+handler.command = ["apk"] 
 export default handler;
