455175c906cfe545f6e666246298a0596a688d9e
flash005 <fbashm085@gmail.com>
2024-09-30 08:32:49 +0200

    Update رد3.js

--- plugins/رد3.js
+++ plugins/رد3.js
@@ -2,12 +2,16 @@ let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = `*قــلــب فـلاش مـن جـوا 👀⚡*`;
-await conn.sendMessage(m.chat, { react: { text: '🙌', key: m.key } })
-    conn.sendFile(m.chat, 'https://envs.sh/06H.jpg', 'image.jpg', message, m);
+    let message = '*مــنــور يـا قــلــب ميدو لــي الـاوامــر اكــتــب (.اوامر) ⚡️*';
+    
+    const emojis = ['⚡️', '🔥', '💥', '✨', '💫', '🌟', '🎉', '👑', '💖', '🎇', '🎆', '❤️', '🌈', '🌹', '🦋'];
+    const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
+    
+    await conn.sendMessage(m.chat, { react: { text: randomEmoji, key: m.key } });
+    conn.sendFile(m.chat, 'https://qu.ax/HAJX.jpg', 'image.jpg', message, m);
 };
 
-handler.customPrefix = /^فلاش$/i;
+handler.customPrefix = /^ميدو$/i;
 handler.command = new RegExp;
 
 export default handler;
