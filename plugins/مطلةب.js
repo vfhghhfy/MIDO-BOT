26e80584c333f7c783cb71ee6e5a0058e773738f
flash005 <fbashm085@gmail.com>
2024-09-30 07:36:17 +0200

    Update مطلةب.js

--- plugins/مطلةب.js
+++ plugins/مطلةب.js
@@ -1,22 +1,21 @@
+
 import uploadImage from '../lib/uploadImage.js'
 
 let handler = async (m) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw '*[❗] یرجی الرد علی صوره*'
+  if (!mime) throw '*رد على صوره المجرم🔒*'
   if (mime && mime.startsWith('video/')) {
-    throw '_خطأ ، الرد فقط علی الصور_';
+    throw '_*حصل ايرور غير متوقع*_';
   }
   let media = await q.download()
   let isTele = /image\/(png|jpe?g|gif)/.test(mime)
   let link = await (isTele ? uploadImage : uploadImage)(media);
-  let lr = (`https://api.popcat.xyz/wanted?image=${link}`)
-  conn.sendFile(m.chat, lr, 'wanted.png', `*📌 تفضل طلبك*`, m)
-  await conn.sendMessage(m.chat, { react: { text: '📍', key: m.key } })
-
+  let api = (`https://api.popcat.xyz/wanted?image=${link}`)
+  conn.sendFile(m.chat, api, 'wanted.png', `*مطلوب حيا او ميتا⚰️*`, m)
 }
 handler.help = ['wanted']
-handler.tags = ['meme']
-handler.command = ['مطلوب','المطلوب']
+handler.tags = ['الاعضاء']
+handler.command = ['مطلوب']
 
 export default handler
