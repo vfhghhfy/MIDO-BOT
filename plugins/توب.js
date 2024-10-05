0fbfde79820eb00686c3606833587e534202799f
flash005 <fbashm085@gmail.com>
2024-10-05 02:42:56 +0200

    Update توب.js

--- plugins/توب.js
+++ plugins/توب.js
@@ -1,24 +1,25 @@
-import util from 'util'
-import path from 'path'
-let user = a => '@' + a.split('@')[0]
-function handler(m, { groupMetadata, command, conn, text, usedPrefix}) {
-if (!text) throw `*مـثـال : .توب متفاعلين*`
-let ps = groupMetadata.participants.map(v => v.id)
-let a = ps.getRandom()
-let b = ps.getRandom()
-let c = ps.getRandom()
-let d = ps.getRandom()
-let e = ps.getRandom()
-let f = ps.getRandom()
-let g = ps.getRandom()
-let h = ps.getRandom()
-let i = ps.getRandom()
-let j = ps.getRandom()
-let k = Math.floor(Math.random() * 70);
-let x = `${pickRandom(['✨','🤍','🔥','💫','😎', '💫', '🤍', '🤍', '✨', '🤍','✨','🤨','💫','🔥','🔥','💫', '🤍','🤍'])}`
-let l = Math.floor(Math.random() * x.length);
-let top = `*${x} توب 10 ${text} ${x}*
-
+import util from 'util';
+import path from 'path';
+const user = (a) => '@' + a.split('@')[0];
+function handler(m, {groupMetadata, command, conn, text, usedPrefix}) {
+  if (!text) throw `مثال على الاستخدام:\n.top *النص*`;
+  const ps = groupMetadata.participants.map((v) => v.id);
+  const a = ps.getRandom();
+  const b = ps.getRandom();
+  const c = ps.getRandom();
+  const d = ps.getRandom();
+  const e = ps.getRandom();
+  const f = ps.getRandom();
+  const g = ps.getRandom();
+  const h = ps.getRandom();
+  const i = ps.getRandom();
+  const j = ps.getRandom();
+  const k = Math.floor(Math.random() * 70);
+  const x = `${pickRandom(['🤓', '😅', '😂', '😳', '😎', '⚡', '😱', '🤑', '🙄', '👀', '🤖', '🤨', '🥴', '🔥', '👇🏻', '😔', '🥶', '🤯'])}`;
+  const l = Math.floor(Math.random() * x.length);
+  const vn = `https://hansxd.nasihosting.com/sound/sound${k}.mp3`;
+  const top = `*${x} أعلى 10 ${text} ${x}*
+    
 *1. ${user(a)}*
 *2. ${user(b)}*
 *3. ${user(c)}*
@@ -28,12 +29,17 @@ let top = `*${x} توب 10 ${text} ${x}*
 *7. ${user(g)}*
 *8. ${user(h)}*
 *9. ${user(i)}*
-*10. ${user(j)}*`.trim()
-conn.sendFile (m.reply (top, null, { mentions: [a, b, c, d, e, f, g, h, i, j]}))}
-handler.help = handler.command = ['توب']
-handler.tags = ['fun']
-handler.group = true
-handler.limit = 0
-export default handler
+*10. ${user(j)}*`;
+  m.reply(top, null, {mentions: [a, b, c, d, e, f, g, h, i, j]});
+  conn.sendFile(m.chat, vn, 'error.mp3', null, m, true, {
+    type: 'audioMessage',
+    ptt: true});
+}
+handler.help = handler.command = ['توب'];
+handler.tags = ['fun'];
+handler.group = true;
+handler.limit = 2;
+export default handler;
 function pickRandom(list) {
-return list[Math.floor(Math.random() * list.length)]}
+  return list[Math.floor(Math.random() * list.length)];
+}
