366c4c7b8831d1e2ee9b17de74075c77d98ca975
flash005 <fbashm085@gmail.com>
2024-10-05 16:25:18 +0200

    Update ترجمه(1).js

--- plugins/ترجمه(1).js
+++ plugins/ترجمه(1).js
@@ -2,18 +2,18 @@ import translate from '@vitalets/google-translate-api'
 const defaultLang = 'ar'
 const tld = 'cn'
 
-let handler = async (m, { args, usedPrefix, command }) => {
+let handler = async (m, { args, usedPrefix, command, emoji }) => {
     let err = `
 ╮ ── ⋆⋆ ── ⋆⋆ ── ⋆⋆ ── ⋆⋆ ── ⋆⋆ ── ⋆⋆ ╭ـ
-˼🤖˹┆ الـاسـم┆⌟𝐅𝐋𝐀𝐒𝐇⌜
+˼🤖˹┆ الـاسـم┆⌟𝑀𝐼𝐷𝛩⌜
 ˼🤖˹┆ مثال┆⌟ .ترجم hello pro⌜
 ╯ ── ⋆⋆ ── ⋆⋆ ── ⋆⋆ ── ⋆⋆ ── ⋆⋆ ── ⋆⋆ ╰ـ
 > ˼👻˹ مــلـاحـــظـــة ⇅ ↶
 ╮ ── ⋆⋆ ── ⋆⋆ ── ⋆⋆ ── ⋆⋆ ── ⋆⋆ ── ⋆⋆ ╭ـ
 > تابع قناة البوت يا برو 👽👇🏻
-https://whatsapp.com/channel/0029VaoUBmSKmCPIIiEatx1H
+https://whatsapp.com/channel/0029VaoNSIY72WU0I4sbeX0G
 ╯ ── ⋆⋆ ── ⋆⋆ ── ⋆⋆ ── ⋆⋆ ── ⋆⋆ ── ⋆⋆ ╰ـ
-> 𝐅𝐋𝐀𝐒𝐇﹝⚡﹞𝐁𝐎𝐓 © 𝐁𝐘 𝐍𝐀𝐑𝐔𝐓𝐎&𝐙𝐀𝐂𝐊`.trim()
+> 𝑀𝐼𝐷𝛩﹝⚡﹞𝐁𝐎𝐓 © 𝐁𝐘 𝑀𝛩𝐻𝐴𝑀𝑀𝐸𝐷 𝐴𝐷𝐸𝐿`.trim()
 
     let lang = args[0]
     let text = args.slice(1).join(' ')
@@ -24,12 +24,13 @@ https://whatsapp.com/channel/0029VaoUBmSKmCPIIiEatx1H
     if (!text && m.quoted && m.quoted.text) text = m.quoted.text
 
     try {
-       let result = await translate(text, { to: lang, autoCorrect: true }).catch(_ => null) 
-       m.reply(result.text)
+        let result = await translate(text, { to: lang, autoCorrect: true }).catch(_ => null)
+        const emojiList = ['😄', '😎', '🤖', '🎉', '💫']
+        const randomEmoji = emojiList[Math.floor(Math.random() * emojiList.length)]
+        m.reply(`${result.text} ${randomEmoji}`)
     } catch (e) {
         throw err
-    } 
-
+    }
 }
 handler.help = ['trad <leng> <text>']
 handler.tags = ['tools']
