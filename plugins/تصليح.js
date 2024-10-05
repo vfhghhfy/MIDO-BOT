ad09271f7b1f3baf834928e76a5962eebbf6e92d
flash005 <fbashm085@gmail.com>
2024-10-05 04:40:49 +0200

    Update تصليح.js

--- plugins/تصليح.js
+++ plugins/تصليح.js
@@ -8,7 +8,7 @@ const handler = async (m, { conn, usedPrefix }) => {
     return conn.sendMessage(m.chat, {text: '*[❗] استخدم هذا الأمر مباشرة في العدد الرئيسي من الروبوت*'}, {quoted: m});
   }
   const chatId = m.isGroup ? [m.chat, m.sender] : [m.sender];
-  const sessionPath = 'GataBotSession';
+  const sessionPath = 'MyvenomSession';
   try {
     const files = await fs.readdir(sessionPath);
     let filesDeleted = 0;
@@ -24,15 +24,15 @@ const handler = async (m, { conn, usedPrefix }) => {
     if (filesDeleted === 0) {
       await conn.sendMessage(m.chat, {text: '*[❗] لم يتم العثور على ملف يتضمن معرف الدردشة*'}, {quoted: m});
     } else {
-      await conn.sendMessage(m.chat, {text: `*┊˹🚀˼┊ تم حذف [ ${filesDeleted} ] ملف من الجلسة┊˹🚀˼┊*`}, {quoted: m});
+      await conn.sendMessage(m.chat, {text: `*[❗] تم حذف ${filesDeleted} ملف من الجلسه بتاعه البوت*`}, {quoted: m});
     }
   } catch (err) {
     console.error('خطأ في قراءة المجلد أو ملفات الجلسة:', err);
     await conn.sendMessage(m.chat, {text: '*[❗] حدث خطأ عند إزالة ملفات الجلسة*'}, {quoted: m});
   }
- await conn.sendMessage(m.chat, {text: '*┊˹🚀˼┊هل تراني الآن يا ياحب؟┊˹🚀˼┊*'}, {quoted: m});
+  await conn.sendMessage(m.chat, {text: `*👋 ¡Hola! Ahora me ves?*\n\n*[❗] اذا لم يرد البوت علي الامر الخاص بك يرجي وضع مثال صغير 3 مرات*\n\n*—◉ مثال:*\n${usedPrefix}s\n${usedPrefix}s\n${usedPrefix}s`}, {quoted: m});
 };
 handler.help = ['fixmsgespera'];
 handler.tags = ['fix'];
-handler.command = /^(صلح|ds)$/i;
+handler.command = /^(تصليح|ds)$/i;
 export default handler;
